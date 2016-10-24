795087d01f0178fdb05f7dedd8add1918a97d0ad
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2016-10-24 22:35:25 +0200

    fixing build stuffs babel loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,25 @@
 
 // fixes issue with long relative paths
 let path = require('path');
-let SRC = path.join(__dirname, './src/');
-let NODE_MODULES = path.join(__dirname, 'node_modules/');
+let SRC = path.join(__dirname, 'src');
+let NODE_MODULES = path.join(__dirname, 'node_modules');
+let DIST = path.join(__dirname, 'dist');
 
 let config = {
-    entry: './src/js/app.js',
+    entry: SRC + '/js/app.js',
     output: {
-        path: './dist/js',
+        path: DIST + '/js/',
         filename: 'app.js'
     },
     devtool: 'inline-source-map',
     module: {
 		loaders: [{
             test: /\.js$/,
-			loader: ['babel', 'babel-loader']
+			loaders: ['babel', 'babel-loader']
         }]
     },
     resolve: {
-        root: [SRC, NODE_MODULES]
+        root: [SRC, NODE_MODULES, DIST]
     }
 }
 
