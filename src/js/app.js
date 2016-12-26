ea70676bc916a2d876f2e60cac360bd76b106281
manidf <mannuel.ferreira@gmail.com>
2016-12-26 23:14:58 +0200

    fixing jquery build

--- src/js/app.js
+++ src/js/app.js
@@ -1,53 +1,53 @@
-function loadData() {
+window.$ = window.jQuery = require("jquery");
 
+var loadDataButton = function () {
+	var searchArticles = document.querySelector('.form-container');
+	searchArticles.submit(loadData);
+}
+
+module.exports = loadDataButton;
+
+function loadData() {
+	console.log('load data function');
     let $body = $('body');
     let $nytHeaderElem = $('.nytimes-header');
     let $nytElem = $('.nytimes-articles');
     let $greeting = $('.greeting');
+	var searchQuery = $('.searchQuery').val();
 
     // clear out old data before new request
     $nytElem.text("");
     $nytHeaderElem.text("");
 
     // Build New York times API ulr
-    let $apiUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+    // let $apiUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 
-    // add api key as parameter
-    $apiUrl += '?' + $.param({
-        'api-key': '2e69849a8c1f4e76aaad0835e3e179cd',
-        'sort': 'newest'
-    });
+    // // add api key as parameter
+    // $apiUrl += '?' + $.param({
+    //     'api-key': '2e69849a8c1f4e76aaad0835e3e179cd',
+    //     'sort': 'newest'
+    // });
 
-    $.ajax({
-        dataType: 'json',
-        url: $apiUrl,
-        method: 'GET'
-    }).done(function(result) {
-        console.log(result);
+	var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchQuery + '&sort=newest&api-key=2e69849a8c1f4e76aaad0835e3e179cd';
+    $.getJSON(nytimesUrl, function(data){
 
-        $nytHeaderElem.text('New York Times Articles About');
+        $nytHeaderElem.text('New York Times Articles About ' + searchQuery);
 
-        let articles = result.response.docs;
-
-		for(let i = 0; i < articles.length; i++) {
-            let article = articles[i];
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
             $nytElem.append('<li class="article">'+
-                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
-                '<p>' + article.snippet + '</p>' +
-                '<small>' + article.pub_date + '</small>' +
-                '</li>');
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
         };
 
-    }).error(function(err){
-        console.log('Sorry there was an error', err);
-        $nytHeaderElem.text('ERROR: articles could not be loaded');
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
 	console.log('APP INIT');
 
-	return false;
 };
 
-
 module.exports = loadData;
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 'use strict';
 
-let webpack = require('webpack');
-let path = require('path'); // fixes issue with long relative paths
-let SRC = path.join(__dirname, 'src'); // set the entry point of the app. SOURCE
-let NODE_MODULES = path.join(__dirname, 'node_modules');
-let DIST = path.join(__dirname, 'dist'); // set the distribution folder. PRODUCTION
+var webpack = require('webpack');
+var path = require('path'); // fixes issue with long relative paths
+var SRC = path.join(__dirname, 'src'); // set the entry point of the app. SOURCE
+var NODE_MODULES = path.join(__dirname, 'node_modules');
+var DIST = path.join(__dirname, 'dist'); // set the distribution folder. PRODUCTION
 
-let config = {
+var config = {
 	entry: {
         app: [SRC + '/js/app.js']
     },
@@ -17,13 +17,14 @@ let config = {
 	resolve: {
 		root: [SRC, NODE_MODULES, DIST],
         alias: {
-            jquery: NODE_MODULES + '/jquery.js'
+            jquery: NODE_MODULES + '/jquery'
         }
     },
 	plugins: [
         new webpack.ProvidePlugin({
-            jQuery: "jquery",
-            'window.jQuery': "jquery"
+            $: "jquery",
+			jQuery: "jquery",
+			"window.jQuery": "jquery"
         }),
 		// build jquery asset from node_modules to distribution directory for consumption in the app.
         new webpack.optimize.CommonsChunkPlugin('vendors', '/vendors/vendors.js', Infinity),
@@ -31,7 +32,8 @@ let config = {
     devtool: 'inline-source-map',
     module: {
 		loaders: [{
-            test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery',
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
 			loaders: ['babel', 'babel-loader']
         }]
     },
