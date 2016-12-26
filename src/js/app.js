24790fcb380ebebaf395cc2b96fbd48af121332e
manidf <mannuel.ferreira@gmail.com>
2016-12-27 00:03:18 +0200

    milligram hello styles

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,9 @@ function loadData() {
     $nytElem.text("");
     $nytHeaderElem.text("");
 
-	var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchQuery + '&sort=newest&api-key=2e69849a8c1f4e76aaad0835e3e179cd';
+	var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+		searchQuery +
+		'&sort=newest&api-key=2e69849a8c1f4e76aaad0835e3e179cd';
 
 	$.getJSON(nytimesUrl, function (data){
 
@@ -19,9 +21,9 @@ function loadData() {
 
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '" class="article-title">' + article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' +
             '</li>');
         };
 
